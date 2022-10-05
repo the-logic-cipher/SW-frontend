18bec039f66eb87e6b4dd72df621e229b02928ac
the-logic-cipher <the.logic.cipher@gmail.com>
2022-10-05 16:09:15 +0500

    navbarstyleadded

--- src/components/layout/Navbar/index.jsx
+++ src/components/layout/Navbar/index.jsx
@@ -3,19 +3,22 @@ import './Navbar.scss'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { Link } from 'react-router-dom'; 
 import Sidebar from '../Sidebar'
+import Badge from '@mui/material/Badge';
 const Navbar = () => {
   return (
     <>
     
     <nav className='container-fluid'>
-      <div className=""><Sidebar />
-        {/* <MenuIcon style={{color:'#243c68'}}/> */}
+      <div className="">
+        <Sidebar />
       </div>
       <div className="">
-        <img src="/Imgs/sw-logo.png" alt="logo" />
+        <Link to='/'><img src="/Imgs/sw-logo.png" alt="logo" /></Link>
       </div>
       <div className="a">
-      <ShoppingCartOutlinedIcon style={{color:'#243c68'}}/>
+      <Badge badgeContent={4} color="primary">
+      <Link to='/cart'><ShoppingCartOutlinedIcon style={{color:'#243c68'}}/></Link>
+      </Badge>
       </div>
       </nav>
     </>
